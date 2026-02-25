87328af4e779bc4e1ea857dbea84bbcc5ead54d0
colinknox <colinwknox@gmail.com>
2026-02-25 07:27:10 -0500

    completed Car class

--- main.py
+++ main.py
@@ -9,10 +9,28 @@ class Vehicle:
     def get_info(self):
         return f"{self.make} {self.model}"
     
+class Car(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
+    
+    def safety_check(self):
+        return "Car safety check: Brakes, lights, and seatbelts OK"   
+    
 
 
 nissan_fairlady = Vehicle("Nissan", "Fairlady")
+car = Car("Ford", "Mustang")
 
-print(f"DEBUG: Make = {nissan_fairlady.make}, Model = {nissan_fairlady.model}")
-print(f"DEBUG: Safety check = {nissan_fairlady.safety_check()}")
-print(f"DEBUG: Get info = {nissan_fairlady.get_info()}")
+# print(f"""
+# Make = {car.make}
+# Model = {car.model}
+# Get info = {car.get_info()}
+# """)
+
+print(f"""
+Make = {car.make}
+Model = {car.model}
+Safety check = {car.safety_check()}
+Get info = {car.get_info()}
+""")
+
